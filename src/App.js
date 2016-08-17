e401f753a5f7bc7c661af029fa9ebc64ce50d59b
Patricio López <lopezjuripatricio@gmail.com>
2016-08-17 00:33:01 -0300

    Add Comments list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: [{
+        title: 'Heya',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      }],
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Lista de comentarios:</h1>
+        <ul>
+          {this.state.comments.map(comment => (
+            <li>
+              <h3>{comment.title}</h3>
+              <p>{comment.body}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
