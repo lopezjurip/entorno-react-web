22e202a0008935dd5c4d622fa8abeb692dbbc71c
Patricio López <lopezjuripatricio@gmail.com>
2016-08-17 00:37:26 -0300

    Fix problem with key in iterator

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
     super(props);
     this.state = {
       comments: [{
+        id: 1,
         title: 'Heya',
         body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       }],
@@ -18,7 +19,7 @@ class App extends Component {
         <h1>Lista de comentarios:</h1>
         <ul>
           {this.state.comments.map(comment => (
-            <li>
+            <li key={comment.id}>
               <h3>{comment.title}</h3>
               <p>{comment.body}</p>
             </li>
